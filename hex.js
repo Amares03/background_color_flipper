08576bebcb5fc3bb776808ce4ba1ced782c69326
Amares03 <amaressabit0314@gmail.com>
2022-05-18 12:41:32 +0300

    set hex color

--- hex.js
+++ hex.js
@@ -1,1 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexNumber = "#";
+  for (let i = 0; i < 6; i++) {
+    hexNumber += hex[i];
+  }
+  document.body.style.backgroundColor = hexNumber;
+  color.textContent = hexNumber;
+});
